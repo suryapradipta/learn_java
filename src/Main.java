e23ea437fdd4ba8cd21baf20e54fb39ce3c472fa
suryapradipta <63577853+suryapradipta@users.noreply.github.com>
2024-02-23 08:42:51 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Ini dari main");
         /*System.out.print("Input your value: ");
 
         int value = input.nextInt();
